3450d431f4e171d728b5da1f9d48ed1ac56b5a93
AaryanshKhera <96333404+AaryanshKhera@users.noreply.github.com>
2021-12-22 11:18:23 +0530

    Add files via upload

---
+++ project10.js
@@ -0,0 +1,19 @@
+
+
+function period()
+  {
+    //uncommet the code to add animation to the ship
+    shipImg1 = loadAnimation("ship-1.png");
+    //shipImg1 = loadAnimation("ship-1.png");
+    //shipImg1 = loadAnimation("ship-1");
+    //shipImg1 = loadAnimation("ship-1.png","ship-2.png",)
+  }
+  //uncommet code to reset the background
+  if(seaX.x<0){
+    //sea.x = 0
+    //sea.xd = sea.width;
+    //sea.x = sea.width/8;
+    //sea.y = height;
+    
+  }
+    
